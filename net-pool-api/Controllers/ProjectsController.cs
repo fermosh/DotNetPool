7512128e417c363b4fbc4ec1460933272b8cf51f
Oscar Legorreta <oscar_legorreta@epam.com>
2017-05-25 17:09:03 -0500

    Moar project svc features

--- net-pool-api/Controllers/ProjectsController.cs
+++ net-pool-api/Controllers/ProjectsController.cs
@@ -23,6 +23,11 @@ namespace net_pool_api.Controllers
                     Name = sm.Name,
                     LastUpdate = sm.Updates.OrderByDescending( u => u.UpdateTime ).Select(_updateProjection).FirstOrDefault()
                 };
+        private readonly Func<Resource,ResourceViewModel> _resourceProjection = sm => new ResourceViewModel{
+                    Id = sm.Id,
+                    Name = sm.Name,
+                    LastOneOnOne = sm.LastOneOnOne
+                };
 
         public ProjectsController(IProjectService projectService){
             _projectService = projectService;
@@ -31,56 +36,65 @@ namespace net_pool_api.Controllers
         [HttpGet]
         public async Task<IEnumerable<ProjectViewModel>> Get()
         {
-            return new ProjectViewModel[] {
-                new ProjectViewModel(),
-                new ProjectViewModel()
-             };
+            var serviceModels = await _projectService.AllProjects();
+            return serviceModels.Select(_projectProjection);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public async Task<ProjectViewModel> Get(Guid id)
         {
-            return new ProjectViewModel();
+            var serviceModel = await _projectService.GetProject(id);
+            return _projectProjection(serviceModel);
         }
 
         // GET api/values/5/Updates
         [HttpGet("{id}/Updates")]
         public async Task<IEnumerable<UpdateViewModel>> GetUpdates(Guid id)
         {
-            return new UpdateViewModel[]{
-                new UpdateViewModel(),
-                new UpdateViewModel(),
-                new UpdateViewModel()
-            };
+            var serviceModels = await _projectService.GetProjectUpdates(id);
+            return serviceModels.Select(_updateProjection);
         }
         // GET api/values/5/Updates
         [HttpGet("{id}/Candidates")]
         public async Task<IEnumerable<ResourceViewModel>> GetCandidates(Guid id)
         {
-            return new ResourceViewModel[]{
-                new ResourceViewModel(),
-                new ResourceViewModel(),
-                new ResourceViewModel()
-            };
+            var serviceModels = await _projectService.GetProjectResources(id);
+            return serviceModels.Select(_resourceProjection);
         }
 
         // POST api/values
         [HttpPost]
-        public async void Post([FromBody]ProjectInputModel value)
+        public async Task<Guid> Post([FromBody]ProjectInputModel value)
         {
+            var newId = await _projectService.AddProject(value.Name,value.Priority,value.Likelyhood);
+            return newId;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public async void Put(Guid id, [FromBody]ProjectInputModel value)
         {
+            await _projectService.UpdateProject(id, value.Name, value.Priority, value.Likelyhood);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public async void Delete(Guid id)
         {
+            await _projectService.DeleteProject(id);
+        }
+        // POST api/Project/5/resourceID
+        [HttpPost("{projectId}/{resourceId}")]
+        public async void AssignResource(Guid projectId,Guid resourceId)
+        {
+            await _projectService.AssignResource(projectId,resourceId);
+        }
+        // DELETE api/Project/5/resourceID
+        [HttpDelete("{projectId}/{resourceId}")]
+        public async void DeassignResource(Guid projectId,Guid resourceId)
+        {
+            await _projectService.DeassignResource(projectId,resourceId);
         }
     }
 }
